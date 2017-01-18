71202d9e993f69c35c1c990cb217d6bc76695dd4
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-01-18 16:59:30 +0700

    Add sample project.json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
   def index
     @header = request.headers["token"]
   end
+
+  def test
+    @body = params
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  post "test", to: "welcome#test"
 end
