ef2e390cc2f8f79a91782483ba52dc2d5501fe47
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-01-18 13:44:21 +0700

    Change rails4 to rails 5

--- config/initializers/swagger.rb
+++
@@ -1,4 +0,0 @@
-GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc.json'
-GrapeSwaggerRails.options.app_name = 'Swagger'
-GrapeSwaggerRails.options.app_url  = ''
-# GrapeSwaggerRails.options.headers['Accept'] = 'application/vnd.product-v1+json'
