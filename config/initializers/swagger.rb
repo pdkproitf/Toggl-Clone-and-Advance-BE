4ff6b4d047db0eff5fd293fa126bf61fb0eaf85a
pdkproitf <phucdaica.pro@gmail.com>
2017-01-19 11:13:05 +0700

    add sign-up success

---
+++ config/initializers/swagger.rb
@@ -0,0 +1,3 @@
+GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc.json'
+GrapeSwaggerRails.options.app_name = 'Swagger'
+GrapeSwaggerRails.options.app_url  = ''
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   root 'welcome#index'
   post "test", to: "welcome#test"
+  mount API::Root => '/'
+  mount GrapeSwaggerRails::Engine => '/swagger'
 end
