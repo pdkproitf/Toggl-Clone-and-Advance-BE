ef3c9ab138a25c2e587fc9080fd19417f2e5aad8
pdkproitf <phucdaica.pro@gmail.com>
2017-01-20 10:00:58 +0700

    fix conflit with @linh  at app/api.rb

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,24 +1,24 @@
 require 'grape-swagger'
 module API
-    class Root < Grape::API
-        format :json
-        # formatter :json #, Grape::Formatter::ActiveModelSerializers
-        use ApiErrorHandler
+  class Root < Grape::API
+    format :json
+    # formatter :json #, Grape::Formatter::ActiveModelSerializers
+    use ApiErrorHandler
 
-        mount UserApi::Registrations
-        mount UserApi::Sessions
-        mount ProjectApi::Projects
-        mount ClientApi::Clients
-        # mount Shop::ProductApi
+    mount UserApi::Registrations
+    mount UserApi::Sessions
+    mount UserApi::Confirmations
+    mount ProjectApi::Projects
+    mount ClientApi::Clients
 
-        add_swagger_documentation(
-            api_version: 'v1',
-            hide_doccumentation_path: false,
-            mount_path: '/api/v1/swagger_doc',
-            hide_format: true,
-            info: {
-                title: 'TRACKING TIME API'
-            }
-        )
-    end
+    add_swagger_documentation(
+      api_version: 'v1',
+      hide_doccumentation_path: false,
+      mount_path: '/api/v1/swagger_doc',
+      hide_format: true,
+      info: {
+        title: 'TRACKING TIME API'
+      }
+    )
+  end
 end
