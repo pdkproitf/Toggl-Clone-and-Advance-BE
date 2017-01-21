0454bd9b500490f897bac9a0ed5ea68e82cdd8aa
Cube <levanlinhcube@gmail.com>
2017-01-21 23:12:37 +0700

    Validate uniqueness name

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,9 @@
 class Project < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
     belongs_to :client
     has_many :project_categories, dependent: :destroy
     has_many :categories, through: :project_categories
     has_many :project_user_roles, dependent: :destroy
     has_many :users, through: :project_user_roles
     has_many :roles, through: :project_user_roles
-
-    validates :name, presence: true, length: { minimum: 6 }
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,5 @@
 class Role < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
     has_many :project_user_roles, dependent: :destroy
     has_many :users, through: :project_user_roles
     has_many :projects, through: :project_user_roles
