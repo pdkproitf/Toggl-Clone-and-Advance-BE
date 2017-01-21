45d1a2233ef0c9e0fb7414a7a9b146cfa037717c
Cube <levanlinhcube@gmail.com>
2017-01-22 03:25:40 +0700

    Create edit a task API

--- app/api/task_api/tasks.rb
+++ app/api/task_api/tasks.rb
@@ -8,6 +8,11 @@ module TaskApi
 
         resource :tasks do
             # => /api/v1/tasks/
+            desc 'Get all tasks'
+            get '/all' do
+                Task.all
+            end
+
             desc 'create new task'
             params do
                 requires :task, type: Hash do
@@ -26,6 +31,27 @@ module TaskApi
                     { error: 'project category user must exist' }
                 end
                 # task
+            end
+
+            desc 'edit a task'
+            params do
+                requires :task, type: Hash do
+                    requires :name, type: String, desc: 'Task name'
+                    requires :project_category_user_id, type: Integer, desc: 'Project category user ID'
+                end
+            end
+            put ':id' do
+                task_params = params['task']
+                task = Task.find(params['id'])
+                begin
+                    task.update(
+                        name: task_params['name'],
+                        project_category_user_id: task_params['project_category_user_id']
+                    )
+                rescue => e
+                    { error: 'project category user must exist' }
+                end
+                task
             end
         end
     end
