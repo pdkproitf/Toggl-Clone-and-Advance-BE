1e3a7d7cb87c0e53c2d0cb2ee4d7b746c1076642
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-02-07 11:49:20 +0700

    Create API get, delete a client

--- app/api/client_api/clients.rb
+++ app/api/client_api/clients.rb
@@ -8,6 +8,21 @@ module ClientApi
 
         resource :clients do
             # => /api/v1/projects/
+            desc 'Get all clients'
+            get '/all' do
+                authenticated!
+                @current_user.clients
+            end
+
+            desc 'Get a client by id'
+            params do
+                requires :id, type: String, desc: 'Client ID'
+            end
+            get ':id' do
+                authenticated!
+                @current_user.clients.where(id: params[:id]).first!
+            end
+
             desc 'create new client'
             params do
                 requires :client, type: Hash do
@@ -22,6 +37,16 @@ module ClientApi
                     name: client_params['name']
                 )
                 client
+            end
+
+            desc 'Delete a client'
+            params do
+                requires :id, type: String, desc: 'Client ID'
+            end
+            delete ':id' do
+                authenticated!
+                client = @current_user.clients.where(id: params[:id]).first!
+                client.destroy
             end
         end
     end
