49223f897a9f069df9c335d1e3344da6e906d973
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-02-09 17:57:56 +0700

    Push timer to corresponding day

--- app/serializers/project_category_serializer.rb
+++ app/serializers/project_category_serializer.rb
@@ -1,6 +1,6 @@
 class ProjectCategorySerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
 
+    has_one :project
     has_one :category
-    # has_many :project_category_users
 end
--- app/serializers/project_category_user_serializer.rb
+++ app/serializers/project_category_user_serializer.rb
@@ -1,5 +1,6 @@
 class ProjectCategoryUserSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
 
-    attributes :id, :project_category_id, :user_id
+    attributes :id
+    has_one :project_category
 end
