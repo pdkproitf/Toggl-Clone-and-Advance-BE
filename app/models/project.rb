770fdc2a6493ecee00cf22f5a200743d42579f23
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-02-14 17:12:10 +0700

    Check if combination project name and client was taken

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true
     belongs_to :user
     belongs_to :client
     has_many :project_categories, dependent: :destroy
