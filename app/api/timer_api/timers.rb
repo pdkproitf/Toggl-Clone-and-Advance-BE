92596697c26bb6aadf4696bc454e7ce1d3f43c04
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-02-16 17:47:58 +0700

    Compare code and fix it

--- app/api/timer_api/timers.rb
+++ app/api/timer_api/timers.rb
@@ -61,17 +61,15 @@ module TimerApi
                 update_task task
             end
 
-          # Check project_category_user_id belong to the current user
-          def is_project_category_current_user pcu_id
-            is_belong = false
-            if ProjectCategoryUser.exists? id: pcu_id
-              pcu = ProjectCategoryUser.find(pcu_id)
-              if pcu.user_id == @current_user.id
-                is_belong = true
-              end
+            # Check project_category_user_id belong to the current user
+            def is_project_category_current_user(pcu_id)
+                is_belong = false
+                if ProjectCategoryUser.exists? id: pcu_id
+                    pcu = ProjectCategoryUser.find(pcu_id)
+                    is_belong = true if pcu.user_id == @current_user.id
+                end
+                is_belong
             end
-            is_belong
-          end
 
         end
 
@@ -85,28 +83,28 @@ module TimerApi
                 end
             end
             get '/' do
-              authenticated!
-              from_day = params[:period][:from_day]
-              to_day = params[:period][:to_day]
-              # timer_list = Timer.joins(task: :project_category_user)
-              # .where(project_category_users: { user_id: @current_user.id })
-              # .where("timers.start_time >= ? AND timers.start_time < ?", from_day, to_day + 1)
+                authenticated!
+                from_day = params[:period][:from_day]
+                to_day = params[:period][:to_day]
 
-              timer_list = Timer.left_outer_joins(task: {project_category_user: {project_category: [:project, :category]}})
-              .where(project_category_users: { user_id: @current_user.id })
-              .where("timers.start_time >= ? AND timers.start_time < ?", from_day, to_day + 1)
-              .select("timers.*", "projects.name")
+                timer_list = Timer.left_outer_joins(task: { project_category_user: { project_category: [:project, :category] } })
+                                  .where(project_category_users: { user_id: @current_user.id })
+                                  .where('timers.start_time >= ? AND timers.start_time < ?', from_day, to_day + 1)
+                                  .select('timers.id', 'timers.start_time', 'timers.stop_time')
+                                  .select('tasks.id as task_id', 'tasks.name as task_name', 'tasks.project_category_user_id as pcu_id')
+                                  .select('projects.name as project_name', 'categories.name as category_name')
+                                  .order('timers.start_time asc')
 
-              data = {}
-              date_list = []
-              timer_list.each do |timer|
-                if !date_list.include?(timer.start_time.to_date.to_s)
-                  date_list.push(timer.start_time.to_date.to_s)
-                  data[timer.start_time.to_date.to_s] = []
+                data = {}
+                date_list = []
+                timer_list.each do |timer|
+                    unless date_list.include?(timer.start_time.to_date.to_s)
+                        date_list.push(timer.start_time.to_date.to_s)
+                        data[timer.start_time.to_date.to_s] = []
+                    end
+                    data[timer.start_time.to_date.to_s].push(timer)
                 end
-                data[timer.start_time.to_date.to_s].push(timer)
-              end
-              data
+                data
             end
 
             desc 'create new timer'
@@ -119,7 +117,7 @@ module TimerApi
                     requires :stop_time, type: DateTime, desc: 'Stop time'
                 end
             end
-            post '/new' do
+            post '/' do
                 authenticated!
                 timer_params = params['timer']
 
@@ -197,7 +195,7 @@ module TimerApi
                 else
                     return error!(I18n.t('task_not_found'), 404)
                 end
-            end # End of add new
+            end
 
             desc 'Edit timer'
             params do
