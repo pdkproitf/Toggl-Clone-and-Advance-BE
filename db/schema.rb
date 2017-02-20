fe4ce4cd060984b4804d7e21fc266dfabe73c1c2
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-02-20 13:46:13 +0700

    Add company reference to Client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220064321) do
+ActiveRecord::Schema.define(version: 20170220064524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170220064321) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "company_id"
+    t.index ["company_id"], name: "index_clients_on_company_id", using: :btree
   end
 
   create_table "companies", force: :cascade do |t|
@@ -139,6 +141,7 @@ ActiveRecord::Schema.define(version: 20170220064321) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
   end
 
+  add_foreign_key "clients", "companies"
   add_foreign_key "companies", "users"
   add_foreign_key "project_categories", "categories"
   add_foreign_key "project_categories", "projects"
