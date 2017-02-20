fbd45f3da8c95410849dfc5823c4b2c1ed771a34
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-02-20 18:02:20 +0700

    Add is_member_report instead of report_permission

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220093743) do
+ActiveRecord::Schema.define(version: 20170220104733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,10 +98,10 @@ ActiveRecord::Schema.define(version: 20170220093743) do
     t.string   "name"
     t.integer  "client_id"
     t.string   "background"
-    t.integer  "report_permission"
-    t.boolean  "is_archived",       default: false
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.boolean  "is_member_report"
+    t.boolean  "is_archived",      default: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.integer  "member_id"
     t.index ["client_id"], name: "index_projects_on_client_id", using: :btree
     t.index ["member_id"], name: "index_projects_on_member_id", using: :btree
