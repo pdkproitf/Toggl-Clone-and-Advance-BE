a529ae36547338a5192a006ae37233c1274484eb
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-02-21 14:32:32 +0700

    Rename billable to is_billable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221041930) do
+ActiveRecord::Schema.define(version: 20170221073033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 20170221041930) do
   create_table "project_categories", force: :cascade do |t|
     t.integer  "project_id"
     t.integer  "category_id"
-    t.boolean  "billable"
+    t.boolean  "is_billable"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_project_categories_on_category_id", using: :btree
