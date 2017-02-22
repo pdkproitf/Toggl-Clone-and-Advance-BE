b2cb2ca0ea387effe95a6d17286d40c4dffcd0c1
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-02-22 15:10:37 +0700

    Change relationship between Project and Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
     validates :name, presence: true, length: { minimum: 1 }
-    has_many :project_categories, dependent: :destroy
-    belongs_to :project, optional: true
+    belongs_to :project
     validates_uniqueness_of :name, scope: :project_id
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,7 @@
 class Project < ApplicationRecord
     belongs_to :member
     has_one :client
-    has_many :project_categories, dependent: :destroy
-    has_many :categories, dependent: :destroy # Create many categories
-    has_many :used_categories, through: :project_categories, source: :category
+    has_many :categories, dependent: :destroy
     has_many :project_members, dependent: :destroy
     has_many :members, through: :project_members
 
