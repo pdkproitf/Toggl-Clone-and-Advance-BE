fbb47d7613b813c45bd27ae3c4f2a4b45fb1955f
pdkproitf <phucdaica.pro@gmail.com>
2017-02-28 15:08:19 +0700

    add validate fo time off

--- app/models/time_off.rb
+++ app/models/time_off.rb
@@ -2,5 +2,12 @@ class TimeOff < ApplicationRecord
     enum status: [:prepare, :pending, :rejected, :approved, :archived]
 
     belongs_to :sender, class_name: 'Member'
-    belongs_to :approver, class_name: 'Member'
+    # belongs_to :approver, class_name: 'Member'
+    validates_presence_of   :start_date, :end_date, :is_start_half_day, :is_end_half_day, :description
+    validate :time_valid
+
+    private
+    def time_valid
+        errors.add(:start_date,'Start time have to less than end time') if self.start_date >= self.end_date
+    end
 end
