1c74b55a396f1245361d53594fcdb12419fe38a2
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-02-28 15:42:02 +0700

    Get recent tasks API done

--- app/api/task_api/tasks.rb
+++ app/api/task_api/tasks.rb
@@ -15,11 +15,11 @@ module TaskApi
             get 'recent' do
                 authenticated!
                 @current_member.timers
-                               .where('category_members.is_archived = ?', false)
-                               .where.not('category_members.category_id IS ?', nil)
-                               .where.not('tasks.name IS ?', nil)
-                               .where('tasks.name IS ?', nil)
+                               .where(category_members: { is_archived: false })
+                               .where.not(category_members: { category_id: nil })
+                               .where.not(tasks: { name: nil })
                                .order('start_time desc')
+                               .limit(params[:number])
             end
         end
     end
