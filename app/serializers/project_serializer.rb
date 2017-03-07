66043df0d2ea658385099d67da45b2399ac7aa4d
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-03-07 10:16:06 +0700

    Return is_pm for members of project

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,6 +1,7 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :background, :client, :tracked_time, :members
-  has_many :members, serializer: MembersSerializer
+  attributes :id, :name, :client, :background,
+             :is_member_report, :tracked_time, :members
+  # has_many :members, serializer: MembersSerializer
 
   def client
     ClientSerializer.new(object.client)
@@ -10,8 +11,20 @@ class ProjectSerializer < ActiveModel::Serializer
     object.get_tracked_time
   end
 
+  # def members
+  #   object.members.where(project_members: { is_archived: false })
+  #   # ActiveModel::Serializer::CollectionSerializer.new(object.members, each_serializer: MembersSerializer)
+  # end
+
   def members
-    object.members.where(project_members: { is_archived: false })
-    # ActiveModel::Serializer::CollectionSerializer.new(object.members, each_serializer: MembersSerializer)
+    list = []
+    object.project_members.where(is_archived: false)
+          .each do |project_member|
+      item = {}
+      item.merge!(MembersSerializer.new(project_member.member))
+      item[:is_pm] = project_member.is_pm
+      list.push(item)
+    end
+    list
   end
 end
