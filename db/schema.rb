52f2a07f8bdb6cde429a2c5dfa9a9b0b2986beab
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-03-07 11:25:03 +0700

    Add column begin_week to company

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302045022) do
+ActiveRecord::Schema.define(version: 20170307041331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,10 +45,11 @@ ActiveRecord::Schema.define(version: 20170302045022) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.string   "domain"
     t.integer  "overtime_max"
+    t.integer  "begin_week",   default: 1
   end
 
   create_table "invites", force: :cascade do |t|
