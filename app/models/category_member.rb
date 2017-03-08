a6673c3bb0b64a9fed9bb485dcd913ee9b6956f1
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-03-08 09:28:57 +0700

    CategoryMember update function tracked time

--- app/models/category_member.rb
+++ app/models/category_member.rb
@@ -1,24 +1,22 @@
 class CategoryMember < ApplicationRecord
-    belongs_to :category, optional: true
-    belongs_to :member
-    has_many :tasks, dependent: :destroy
-    validates_uniqueness_of :category_id, scope: :member_id, if: 'category_id.present?'
+  belongs_to :category, optional: true
+  belongs_to :member
+  has_many :tasks, dependent: :destroy
+  validates_uniqueness_of :category_id, scope: :member_id, if: 'category_id.present?'
 
-    def tracked_time
-        sum = 0
-        if tasks
-            tasks.each do |task|
-                sum += task.tracked_time
-            end
-        end
-        sum
+  def tracked_time(begin_date, end_date)
+    sum = 0
+    tasks.each do |task|
+      sum += task.tracked_time(begin_date, end_date)
     end
+    sum
+  end
 
-    def archive
-        update_attributes(is_archived: true)
-    end
+  def archive
+    update_attributes(is_archived: true)
+  end
 
-    def unarchive
-        update_attributes(is_archived: false)
-    end
+  def unarchive
+    update_attributes(is_archived: false)
+  end
 end
