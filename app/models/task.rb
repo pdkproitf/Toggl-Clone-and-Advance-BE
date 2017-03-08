44efaf4391666aed935cb6bffc285e9eab31516b
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-03-08 09:18:57 +0700

    Task trackted time in specific period of time

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,12 +4,16 @@ class Task < ApplicationRecord
   # validates :name, length: { minimum: 1 }, allow_nil: true
   validates_uniqueness_of :name, scope: :category_member_id, if: 'name.present?'
 
-  def tracked_time
+  def tracked_time(begin_date, end_date)
     sum = 0
-    if timers
-      timers.each do |timer|
-        sum += timer.tracked_time
-      end
+    if !begin_date.nil? && !end_date.nil?
+      timer_list = timers.where('start_time >= ? AND start_time < ?',
+                                begin_date, end_date + 1)
+    else
+      timer_list = timers
+    end
+    timer_list.each do |timer|
+      sum += timer.tracked_time
     end
     sum
   end
