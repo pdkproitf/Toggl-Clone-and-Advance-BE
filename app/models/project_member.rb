674684d63bf4b20c4a1fc9399cba0ec00173666e
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-03-09 11:54:12 +0700

    Update archive and unarchive function in ProjectMember

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -6,10 +6,26 @@ class ProjectMember < ApplicationRecord
   validates_uniqueness_of :project_id, scope: :member_id, if: 'project_id.present?'
 
   def archive
+    return if is_archived == true
+    category_members.each do |category_member|
+      if category_member[:is_archived_by_project_member] == is_archived
+        category_member.archived_by_project_member
+      else
+        category_member.unarchived_by_project_member
+      end
+    end
     update_attributes(is_archived: true)
   end
 
   def unarchive
+    return if is_archived == false
+    category_members.each do |category_member|
+      if category_member[:is_archived_by_project_member] == is_archived
+        category_member.unarchived_by_project_member
+      else
+        category_member.archived_by_project_member
+      end
+    end
     update_attributes(is_archived: false)
   end
 end
