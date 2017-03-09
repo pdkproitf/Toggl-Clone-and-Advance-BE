551438e5652ab4e9441ac48101203b59ad7a1071
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-03-09 16:14:04 +0700

    Update tracked_time in MembersSerializer

--- app/serializers/members_serializer.rb
+++ app/serializers/members_serializer.rb
@@ -20,12 +20,12 @@ class MembersSerializer < ActiveModel::Serializer
 
   def tracked_time
     sum = 0
-    category_members = object.category_members
-                             .where.not(category_id: nil)
-                             .where(projects: { is_archived: false })
-                             .where(categories: { is_archived: false })
-                             .where(category_members: { is_archived: false })
-                             .joins(category: :project)
+    category_members = object
+                       .category_members
+                       .where.not(category_id: nil)
+                       .where(category_members: { is_archived_by_category: false })
+                       .where(category_members: { is_archived_by_project_member: false })
+                       .joins(category: :project)
     category_members.each do |category_member|
       sum += category_member.tracked_time(@begin_date, @end_date)
     end
