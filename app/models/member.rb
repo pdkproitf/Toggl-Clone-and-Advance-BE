be04f30eb22ecb50a622cecbefa82190926e3973
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-03-23 09:27:06 +0700

    Remove assigned_categories function

--- app/models/member.rb
+++ app/models/member.rb
@@ -96,21 +96,6 @@ class Member < ApplicationRecord
     true
   end
 
-  def assigned_categories
-    category_members
-      .where.not(category_id: nil)
-      .where(is_archived_by_category: false)
-      .where(is_archived_by_project_member: false)
-      .select('category_members.id')
-      .select('projects.id as project_id', 'projects.name as project_name')
-      .select('projects.background')
-      .select('clients.id as client_id', 'clients.name as client_name')
-      .select('categories.id as category_id', 'categories.name as category_name')
-      .select('category_members.id as category_member_id')
-      .joins(category: { project: :client })
-      .order('projects.id desc', 'categories.id asc')
-  end
-
   def perfect_tasks
     tasks.where.not(category_members: { category_id: nil })
          .where(category_members: { is_archived_by_category: false })
