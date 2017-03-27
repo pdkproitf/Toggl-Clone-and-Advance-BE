7b995b4f822314611e6cdb56e416ca71e59f584d
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-03-27 10:46:32 +0700

    Fix recent tasks finish

--- app/serializers/recent_task_serializer.rb
+++ app/serializers/recent_task_serializer.rb
@@ -1,28 +1,16 @@
 class RecentTaskSerializer < ActiveModel::Serializer
   attributes :id, :name, :category_member_id, :project_name,
-             :category_name, :background, :last_stop_time
+             :category_name, :background
 
   def project_name
-    object.category_member.category.project.name if category?
+    object.category_member.category.project.name
   end
 
   def category_name
-    object.category_member.category.name if category?
+    object.category_member.category.name
   end
 
   def background
-    object.category_member.category.project.background if category?
-  end
-
-  def category?
-    if object.category_member.category
-      true
-    else
-      false
-    end
-  end
-
-  def last_stop_time
-    object.timers.order('stop_time desc').first.stop_time
+    object.category_member.category.project.background
   end
 end
