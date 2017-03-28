158418dc2c7744ebb5c77938e58d931d6e7a9c3d
pdkproitf <phucdaica.pro@gmail.com>
2017-03-28 15:01:35 +0700

    add member api

--- app/api/member_api/members.rb
+++ app/api/member_api/members.rb
@@ -23,6 +23,36 @@ module MemberApi
                 error!(I18n.t("access_denied"), 403) unless able_see_member?(member)
                 return_message(I18n.t('success'), MembersSerializer.new(member))
             end
+
+            desc 'edit member inform: role, jobs, role'
+            params do
+                requires :members, type: Hash do
+                    requires :role_id, type: Integer, desc: 'member role'
+                    requires :jobs, type: Array[Integer], desc: 'array job id'
+                end
+            end
+            post do
+                member = Member.find(params[:id])
+                error!(I18n.t("access_denied"), 403) unless able_modify_member?(member)
+
+                update_role( params[:members][:role_id])
+                update_jobs(params[:members][:jobs], member)
+                return_message(I18n.t('success'), MembersSerializer.new(member))
+            end
+
+            after do
+                archived_member_in_project(@member)
+            end
+
+            desc 'reject member from company'
+            delete ':id' do
+                @member = Member.find(params[:id])
+                error!(I18n.t("access_denied"), 403) unless able_modify_member?(@member)
+                error!(I18n.t("member.errors.already_archived"), 400) unless @member.actived?
+                @member.update_attributes(is_archived: true)
+                status 200
+                return_message(I18n.t('success'))
+            end
         end
     end
 end
