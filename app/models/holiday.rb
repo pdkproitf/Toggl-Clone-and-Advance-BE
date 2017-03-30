a8253f6b8c949f8a3071a18d20dfe7847cf1344a
pdkproitf <phucdaica.pro@gmail.com>
2017-03-30 08:48:52 +0700

    create validate days concern for timeoff and holiday

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -3,43 +3,23 @@ class Holiday < ApplicationRecord
 
     validates :name, length: { minimum: 2 }
     validates_presence_of :begin_date, :end_date, :company_id
-    validate :days_valid
-    validate :conflict_date, on: [:create, :update]
+    validate :valid_start_end_days
+    validate :conflict_holiday, on: :create
+    validate :conflict_holiday_update, on: :update
 
     private
+    include DaysValidates
+
     # begin_date_cannot_be_greater_than_end_date
-    def days_valid
-        errors.add(:begin_date, I18n.t("less_than_end_date")) if begin_date > end_date
+    def valid_start_end_days
+        days_valid?(begin_date, end_date, 'begin_date')
     end
 
-    def conflict_date(id = nil)
-        conflict_start = conflict_begin_date(id)
-        errors.add(:begin_date, I18n.t("already_request")) unless conflict_start.blank?
-
-        conflict_end = conflict_end_date(id)
-        errors.add(:end_date, I18n.t("already_request")) unless conflict_end.blank?
-
-        conflict_middle_date(id) if conflict_start.blank? && conflict_end.blank?
+    def conflict_holiday
+        conflict_date?(Holiday.where(company_id: company_id), 'begin_date', begin_date, end_date)
     end
 
-    def conflict_date_update
-        conflict_date(id)
-    end
-
-    def conflict_begin_date(id)
-        Holiday.where('begin_date <= ? and end_date >= ? and company_id = ? and id != ?',
-            begin_date, begin_date, company_id, id)
-    end
-
-    def conflict_end_date(id)
-        Holiday.where('begin_date <= ? and end_date >= ? and company_id = ? and id != ?',
-            end_date, end_date, company_id, id)
-    end
-
-    def conflict_middle_date(id)
-        conflict_middle = Holiday.where('((begin_date >= ? and begin_date <= ?) or
-            (end_date >= ? and end_date <= ?)) and company_id = ? and id != ?',
-            begin_date, end_date, begin_date, end_date, company_id, id)
-        errors.add(:begin_date, I18n.t("already_request")) unless conflict_middle.blank?
+    def conflict_holiday_update
+        conflict_date?(Holiday.where(company_id: company_id), 'begin_date', begin_date, end_date, id)
     end
 end
--- app/models/time_off.rb
+++ app/models/time_off.rb
@@ -5,58 +5,35 @@ class TimeOff < ApplicationRecord
     belongs_to :approver, class_name: 'Member'
 
     validates_presence_of :start_date, :end_date, :description, :sender_id
-    validate :days_valid
+    validate :valid_start_end_days
     validate :conflict_timeoff, on: :create
     validate :conflict_timeoff_update, on: :update
 
     def send_email send_mail_to, current_member = nil
         send_mail_to.each do |member|
             TimeOffMailer.timeoff_announce(self, member.user.email, current_member)
-            .deliver_later(wait: Settings.send_later.seconds)
+                .deliver_later(wait: Settings.send_later.seconds)
         end
     end
 
     private
+    include DaysValidates
+
     # begin_date_cannot_be_greater_than_end_date
-    def days_valid
-        errors.add(:start_date, I18n.t("less_than_end_date")) if start_date > end_date
+    def valid_start_end_days
+        days_valid?(start_date, end_date, 'start_date')
         errors.add(:start_date, I18n.t("over_date")) if start_date < Time.now.beginning_of_day
     end
 
     def conflict_timeoff_update
         status = [TimeOff.statuses[:rejected]]
         status.push(TimeOff.statuses[:pending]) if approver_id_changed?
-        conflict_timeoff status, id
+        conflict_timeoff(status, id)
     end
 
-    def conflict_timeoff status = [TimeOff.statuses[:rejected]], id = 0
+    def conflict_timeoff(status = [TimeOff.statuses[:rejected]], id = 0)
         return true if status == TimeOff.statuses[:rejected]
 
-        conflict_start = conflict_start_date(status, id)
-        errors.add(:start_date, I18n.t("already_request")) unless conflict_start.blank?
-
-        conflict_end = conflict_end_date(status, id)
-        errors.add(:end_date, I18n.t("already_request")) unless conflict_end.blank?
-
-        conflict_middle_date(status, id) if conflict_start.blank? && conflict_end.blank?
-    end
-
-    def conflict_start_date(status, id)
-        sender.off_requests
-            .where('start_date <= ? and end_date >= ? and status NOT IN (?) and id != ?',
-                start_date, start_date, status, id)
-    end
-
-    def conflict_end_date(status, id)
-        sender.off_requests
-            .where('(start_date <= ? and end_date >= ? and status NOT IN (?) and id != ?)',
-                end_date, end_date, status, id)
-    end
-
-    def conflict_middle_date(status, id)
-        conflict_middle = sender.off_requests
-            .where('(((start_date >= ? and start_date <= ?) or (end_date >= ? and end_date <= ?))
-                and status NOT IN (?) and id != ?)', start_date, end_date, start_date, end_date, status, id)
-        errors.add(:start_date, I18n.t("already_request")) unless conflict_middle.blank?
+        conflict_date?(sender.off_requests.where.not(status: status), 'start_date', start_date, end_date, id)
     end
 end
