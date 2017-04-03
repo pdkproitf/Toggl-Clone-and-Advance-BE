516a97e000c6a99ca482981e8e9056200b56cfff
pdkproitf <phucdaica.pro@gmail.com>
2017-04-03 13:38:37 +0700

    validate timeoff weekend and holiday

--- app/models/concerns/days_validates.rb
+++ app/models/concerns/days_validates.rb
@@ -1,32 +1,31 @@
 module DaysValidates extend ActiveSupport::Concern
-    def days_valid?(begin_date, end_date, field)
-        errors.add("#{field}", I18n.t("less_than_end_date")) if begin_date > end_date
+    # start date have to less than end date
+    def days_valid?(begin_date, begin_field, end_date)
+        errors.add("#{begin_field}", I18n.t("less_than_end_date")) if begin_date > end_date
     end
 
-    def conflict_date?(records, field, begin_date, end_date, id = 0)
-        conflict_start = conflict_begin_date(id, records, field, begin_date)
-        errors.add("#{field}", I18n.t("already_request")) unless conflict_start.blank?
-
-        conflict_end = conflict_end_date(id, records, field, begin_date)
-        errors.add(:end_date, I18n.t("already_request")) unless conflict_end.blank?
-
-        conflict_middle_date(id, records, field, begin_date) if conflict_start.blank? && conflict_end.blank?
-    end
-
-    def conflict_begin_date(id, records, field, begin_date)
-        records.where("#{field} <= ? and end_date >= ? and id != ?",
+    def conflict_begin_date(id, records, begin_date, begin_field, end_date, end_field)
+        records.where("#{begin_field} <= ? and #{end_field} >= ? and id != ?",
             begin_date, begin_date, id)
     end
 
-    def conflict_end_date(id, records, field, begin_date)
-        records.where("#{field} <= ? and end_date >= ? and id != ?",
+    def conflict_end_date(id, records, begin_date, begin_field, end_date, end_field)
+        records.where("#{begin_field} <= ? and #{end_field} >= ? and id != ?",
             end_date, end_date, id)
     end
 
-    def conflict_middle_date(id, records, field, begin_date)
-        conflict_middle = records.where("((#{field} >= ? and #{field} <= ?) or
-            (end_date >= ? and end_date <= ?)) and id != ?",
+    def conflict_middle_date(id, records, begin_date, begin_field, end_date, end_field)
+        conflict_middle = records.where("((#{begin_field} >= ? and #{begin_field} <= ?) or
+            (#{end_field} >= ? and #{end_field} <= ?)) and id != ?",
             begin_date, end_date, begin_date, end_date, id)
-        errors.add(:days, I18n.t("already_request")) unless conflict_middle.blank?
+    end
+
+    def constraint_weekend?(begin_date, end_date)
+        (begin_date.to_i .. end_date.to_i).step(1.day).each do |day|
+            if Time.at(day).on_weekend?
+                return true
+            end
+        end
+        false;
     end
 end
--- app/models/time_off.rb
+++ app/models/time_off.rb
@@ -6,8 +6,9 @@ class TimeOff < ApplicationRecord
 
     validates_presence_of :start_date, :end_date, :description, :sender_id
     validate :valid_start_end_days
-    validate :conflict_timeoff, on: :create
-    validate :conflict_timeoff_update, on: :update
+    validate :conflict_timeoff, :conflict_holiday, on: :create
+    validate :conflict_timeoff_update, :conflict_holiday_update, on: :update
+    validate :constraint_weekend
 
     def send_email send_mail_to, current_member = nil
         send_mail_to.each do |member|
@@ -21,7 +22,7 @@ class TimeOff < ApplicationRecord
 
     # begin_date_cannot_be_greater_than_end_date
     def valid_start_end_days
-        days_valid?(start_date, end_date, 'start_date')
+        days_valid?(start_date, 'start_date', end_date)
         errors.add(:start_date, I18n.t("over_date")) if start_date < Time.now.beginning_of_day
     end
 
@@ -33,7 +34,39 @@ class TimeOff < ApplicationRecord
 
     def conflict_timeoff(status = [TimeOff.statuses[:rejected]], id = 0)
         return true if status == TimeOff.statuses[:rejected]
+        records = sender.off_requests.where.not(status: status)
 
-        conflict_date?(sender.off_requests.where.not(status: status), 'start_date', start_date, end_date, id)
+        conflict_start = conflict_begin_date(id, records, start_date, 'start_date', end_date, 'end_date')
+        errors.add("start_date", I18n.t("already_request")) unless conflict_start.blank?
+
+        conflict_end = conflict_end_date(id, records, start_date, 'start_date', end_date, 'end_date')
+        errors.add("end_date", I18n.t("already_request")) unless conflict_end.blank?
+
+        if conflict_start.blank? && conflict_end.blank?
+            conflict_middle = conflict_middle_date(id, records, start_date, 'start_date', end_date, 'end_date')
+            errors.add(:days, I18n.t("already_request")) unless conflict_middle.blank?
+        end
+    end
+
+    def constraint_weekend
+        errors.add(:days, I18n.t("timeoff.errors.weekend")) if constraint_weekend?(start_date, end_date)
+    end
+
+    def conflict_holiday_update
+        conflict_holiday(id)
+    end
+
+    def conflict_holiday(id =0)
+        records = sender.company.holidays
+        conflict_start = conflict_begin_date(id, records, start_date, 'begin_date', end_date, 'end_date')
+        errors.add("start_date", I18n.t('timeoff.errors.holiday')) unless conflict_start.blank?
+
+        conflict_end = conflict_end_date(id, records, start_date, 'begin_date', end_date, 'end_date')
+        errors.add("end_date", I18n.t("timeoff.errors.holiday")) unless conflict_end.blank?
+
+        if conflict_start.blank? && conflict_end.blank?
+            conflict_middle = conflict_middle_date(id, records, start_date, 'begin_date', end_date, 'end_date')
+            errors.add(:days, I18n.t("timeoff.errors.holiday")) unless conflict_middle.blank?
+        end
     end
 end
