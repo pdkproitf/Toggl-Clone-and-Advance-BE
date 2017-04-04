0dab91b6ada237f196f1f4b1a101cbcfa15ada76
pdkproitf <phucdaica.pro@gmail.com>
2017-04-04 14:03:16 +0700

    validate for role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,6 @@
 class Role < ApplicationRecord
     has_many :members, -> {where(is_archived: false)}
+
+    validates :name, presence: true, length: { minimum: Settings.role_min_length },
+                     uniqueness: { case_sensitive: false }
 end
