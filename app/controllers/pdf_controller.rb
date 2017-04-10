2bd04cd72856eb5d5ba375cfab6cb827f1c2215e
Le Van Linh <lynleva.cube.it141@gmail.com>
2017-04-10 12:17:33 +0700

    Define zip_folder function in PdfHelper

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,6 +1,8 @@
 class PdfController < ApplicationController
+  require 'zip'
+  include PdfHelper
   def show
-    @projects = Project.all
+    @projects = Project.all.order(:name)
     @projects.each do |project|
       # respond_to do |format|
       #   format.html
@@ -13,8 +15,22 @@ class PdfController < ApplicationController
       # end
 
       pdf = Prawn::Document.new
-      pdf.text(project.name.to_s + project.client.name.to_s)
+      pdf.text(project.name.to_s)
       pdf.render_file("tmp/report_pdfs/#{project.name}.pdf")
     end
+    folder = 'tmp/report_pdfs/'
+    zipfile_name = 'tmp/reports.zip'
+    # input_filenames = Dir.entries('tmp/report_pdfs/').select { |f| !File.directory? f }
+    # Zip.continue_on_exists_proc = true
+    # Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+    #   input_filenames.each do |filename|
+    #     zipfile.add(filename, folder + '/' + filename)
+    #   end
+    #   zipfile.get_output_stream('myFile') { |os| os.write 'myFile contains just this' }
+    # end
+    zip_folder(folder, zipfile_name)
+    send_file zipfile_name, type: 'application/zip',
+                            disposition: 'attachment',
+                            filename: 'reports.zip'
   end
 end
--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -1,2 +1,12 @@
 module PdfHelper
+  def zip_folder(folder, zipfile_name)
+    input_filenames = Dir.entries("#{folder}/").select { |f| !File.directory? f }
+    Zip.continue_on_exists_proc = true
+    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+      input_filenames.each do |filename|
+        zipfile.add(filename, folder + '/' + filename)
+      end
+      zipfile.get_output_stream('myFile') { |os| os.write 'myFile contains just this' }
+    end
+  end
 end
